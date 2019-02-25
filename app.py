c7d34e400c62534668e15ed83663abfcfd945f68
pupilye <zhilin_ye@outlook.com>
2019-02-24 17:23:22 -0700

    DATABASE_URL

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') # Read the environment variable
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') # Read the environment variable
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # turn off the Flask one, not the core one
 app.secret_key = 'jose'
 api = Api(app)
